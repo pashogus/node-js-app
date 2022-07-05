1495815c9311a986df1fc2e56aa8d41a955e5e2a
Pashogus <shansasi.28@gmail.com>
2022-07-05 23:44:17 +0530

    changes in index.js for port

--- index.js
+++ index.js
@@ -5,10 +5,12 @@ const rateController = require('./Controller/rateController.js')
 
 var app = express();
 
-app.listen(3000, ()=>
-{
-console.log("Express started at port 3000");
-});
+const server = app.listen(process.env.PORT || 5000, () => {
+    const port = server.address().port;
+    console.log(`Express is working on port ${port}`);
+  });
+
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use('/rate',rateController);
